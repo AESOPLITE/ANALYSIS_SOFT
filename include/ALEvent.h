3e41485f23acafe20611c5a14e1cc5a02cf8ed1e
smechbal <smechbal@ucsc.edu>
2017-05-15 16:35:05 -0700

    Update ALEvent.h

--- include/ALEvent.h
+++ include/ALEvent.h
@@ -2,65 +2,42 @@
 ///    Author: Pierre-Simon Mangeard, psmangeard@gmail.com
 ///    Department of Physics and Astronomy, University of Delaware, October 28, 2016
 ////////////////////////////////////////////////////////////////////////////////////////// 
-
+#ifndef __ALEVENT__
+#define __ALEVENT__
+#include "headers.h"
 #include "ALTckhit.h"
 
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <algorithm>
-#include <fstream>
-#include <stdio.h>
-#include <string>
-#include <stdlib.h>
-#include <sstream>
-#include <TROOT.h>
-#include <Riostream.h>
-#include "TChain.h"
-#include "TCanvas.h"
-#include "TObject.h"
-#include "TGraph.h"
-#include "TGraphErrors.h"
-#include "TMultiGraph.h"
-#include "TAxis.h"
-#include "TMath.h"
-#include "TLegend.h"
-#include "TLeaf.h"
-#include "TLine.h"
-#include "TCanvas.h"
-#include "TStyle.h"
-#include "TLinearFitter.h"
-#include "TH2F.h"
-#include "TH2D.h"
-#include "TCut.h"
-#include "TLine.h"
-#include "TArrow.h"
-#include "TBox.h"
-#include "TPDF.h"
-#include "TTree.h"
-#include "TBranch.h"
-#include "TFile.h"
-#include "TPostScript.h"
-#include "TPaveText.h"
-#include "TString.h"
-#include "TH1F.h"
-#include "TSystem.h"
-#include "THStack.h"
-#include "TNtuple.h"
-#include "TFormula.h"
-#include "TFitResultPtr.h"
-#include "TFitResult.h"
-#include "TH3F.h"
-#include "TRandom3.h"
-#include "TRandom2.h"
-#include "TRandom1.h"
-
-
-using namespace std;
-
-#ifndef ALEVENT_H
-#define ALEVENT_H
-
+/*
+struct ALHit //Track or hits
+{
+ float mregMC; // region if MC
+ float mtrackMC; // mtrack of track
+ int typeMC;   //type of particle if MC
+ float eMC;     //total energy if MC (entrance of the tracker layer)
+ float xin;      //x coordinate of hit MC (entrance of the tracker layer)
+ float yin;      //y coordinate of hit MC (entrance of the tracker layer)
+ float zin;      //z coordinate of hit MC (entrance of the tracker layer)
+ float xout;     //x coordinate of hit MC (exit the tracker layer)
+ float yout;     //y coordinate of hit MC (exit the tracker layer)
+ float zout;     //z coordinate of hit MC (exit the tracker layer)
+ float age;    //Age of particle if MC  (entrance of the tracker layer)
+ float cx;     //cosineX of momentum MC (Average in the tracker layer)
+ float cy;     //cosineY of momentum MC (Average in the tracker layer)
+ float cz;     //cosineZ of momentum MC (Average in the tracker layer)
+ int flag;     //flag value: Track=1, Hit=0
+ float DeltaE;     //Energy along the track or at hit location Hit=0
+ //Reconstructed information
+ float xreco;      //x coordinate of hit 
+ float yreco;      //y coordinate of hit 
+ float zreco;      //z coordinate of hit 
+ float agereco;    //Age of particle  
+ float cxreco;     //cosineX of momentum 
+ float cyreco;     //cosineY of momentum 
+ float czreco;     //cosineZ of momentum    
+ float ereco;     //kinetic energy 
+ int k;     //kth hit in event 
+};
+*/
 
 class ALEvent:public TObject
 {
@@ -73,20 +50,23 @@ class ALEvent:public TObject
    double EkMC;   //kinetic energy of the particle
    double X0MC,Y0MC,Z0MC;//Coordinates of the partcle at the injection point 
    double CX0MC,CY0MC,CZ0MC; //Incidence cosines of the partcle at the injection point 
-   
    // Hits information
    int Nhits; //Number of hits in the event
    
    //Reconstruction information: variables finish with reco
    int typereco; //type of particle
-   double Ekreco;   //kinetic energy of the particle
+   double Ekreco, p0reco;   //kinetic energy and momentum of the particle
    double X0reco,Y0reco,Z0reco;//Coordinates of the partcle at the injection point 
    double CX0reco,CY0reco,CZ0reco; //Incidence cosines of the partcle at the injection point 
+   int ndf;					 // number of degrees of freedom
+   double chi2, cl;			 //chi2 of fit and confidence level (cl = Prob(chi2, ndf)
+   double d0, phi0, cpa, dz, tanl;				//reconstructed helical track parameters
+   double d0err2, phi0err2, cpaerr2, dzerr2, tanlerr2; 		//err^2 of track parameters
    
    //Hits information
    std::vector<ALTckhit*> hits;  
    
-   //Triggers
+   //External Triggers
    bool T1;
    bool T2;
    bool T3;
@@ -102,11 +82,21 @@ class ALEvent:public TObject
    std::vector<double> timeT3;
    std::vector<double> timeT4;
    std::vector<double> timeg;     
+   //Internal Triggers 
+   //Tracker layer for 0 to 6. top layer is 0
+   //integer value coded with 7 bits values
+   //\Sum\Limits_{k=0}^{6} 2^k
+   int Ti=0;
+   
  public: 
    //Constructors
    ALEvent();// Default
    //Destructor
    ~ALEvent(){};   
+   ///////////////////////////////
+   // Methods
+   ///////////////////////////////
+   void Copy(ALEvent*);
    ////////////////////////////////
    //"setting" member methods
    ////////////////////////////////
@@ -126,12 +116,27 @@ class ALEvent:public TObject
    ////////////////////////////////
    void set_typereco(int a){typereco=a;}
    void set_Ekreco(double a){Ekreco=a;}
+   void set_p0reco(double a){p0reco=a;}
    void set_X0reco(double a){X0reco=a;}
    void set_Y0reco(double a){Y0reco=a;}
    void set_Z0reco(double a){Z0reco=a;}
    void set_CX0reco(double a){CX0reco=a;}
    void set_CY0reco(double a){CY0reco=a;}
    void set_CZ0reco(double a){CZ0reco=a;}
+   void set_ndf(int a){ndf=a;}
+   void set_chi2(double a){chi2=a;}
+   void set_cl(double a){cl=a;}
+   void set_d0(double a){d0=a;}
+   void set_phi0(double a){phi0=a;}
+   void set_cpa(double a){cpa=a;}
+   void set_dz(double a){dz=a;}
+   void set_tanl(double a){tanl=a;}
+   void set_d0err2(double a){d0err2=a;}
+   void set_phi0err2(double a){phi0err2=a;}
+   void set_cpaerr2(double a){cpaerr2=a;}
+   void set_dzerr2(double a){dzerr2=a;}
+   void set_tanlerr2(double a){tanlerr2=a;}
+   
    void add_hit(ALTckhit* h){hits.push_back(h);Nhits++;}
    void set_T1(bool a){T1=a;}
    void set_T2(bool a){T2=a;}
@@ -147,6 +152,7 @@ class ALEvent:public TObject
    void add_timeT3(double a){timeT3.push_back(a);}
    void add_timeT4(double a){timeT4.push_back(a);}
    void add_timeg(double a){timeg.push_back(a);}   
+   void set_Ti(int a){Ti=a;}
    
    ////////////////////////////////
    //"Getting" member methods
@@ -161,17 +167,33 @@ class ALEvent:public TObject
    double get_CX0MC(){return CX0MC;}
    double get_CY0MC(){return CY0MC;}
    double get_CZ0MC(){return CZ0MC;}
+
+
    ////////////////////////////////
    int get_Nhits(){return Nhits;}
    ////////////////////////////////
    int get_typereco(){return typereco;}
    double get_Ekreco(){return Ekreco;}
+   double get_p0reco(){return p0reco;}
    double get_X0reco(){return X0reco;}
    double get_Y0reco(){return Y0reco;}
    double get_Z0reco(){return Z0reco;}
    double get_CX0reco(){return CX0reco;}
    double get_CY0reco(){return CY0reco;}
    double get_CZ0reco(){return CZ0reco;}
+   int get_ndf(){return ndf;}
+   double get_chi2(){return chi2;}
+   double get_cl(){return cl;}
+   double get_d0(){return d0;}
+   double get_phi0(){return phi0;}
+   double get_cpa(){return cpa;}
+   double get_dz(){return dz;}
+   double get_tanl(){return tanl;}
+   double get_d0err2(){return d0err2;}
+   double get_phi0err2(){return phi0err2;}
+   double get_cpaerr2(){return cpaerr2;}
+   double get_dzerr2(){return dzerr2;}
+   double get_tanlerr2(){return tanlerr2;}
    std::vector<ALTckhit*> get_hits(){return hits;}
    bool get_T1(){return T1;}
    bool get_T2(){return T2;}
@@ -188,6 +210,7 @@ class ALEvent:public TObject
    std::vector<double>  get_timeT3(){return timeT3;}
    std::vector<double>  get_timeT4(){return timeT4;}
    std::vector<double>  get_timeg(){return timeg;}
+   int get_Ti(){return Ti;}
 
    ////////////////////////////////
    ClassDef(ALEvent,1)
