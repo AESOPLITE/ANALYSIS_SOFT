c79ea72cec55bb56aa2e2c8f0dec8c6fe0f5e7f4
smechbal <smechbal@ucsc.edu>
2017-08-05 16:15:41 -0700

    Assign randomly big value to fSigmaY

--- src/ALKalman/ALKalman.h
+++ src/ALKalman/ALKalman.h
@@ -1,3 +1,4 @@
+
 #ifndef __ALKALMAN__
 #define __ALKALMAN__
 
@@ -14,12 +15,68 @@
 #include "ALMeasLayer.h"	  // from ALKalman
 #include "ALKalDetector.h"	  // from ALKalman
 #include "ALHit.h"			  // from ALKalman
-#include "TNtupleD.h"		  // from ROOT
 #include "TFile.h"			  // from ROOT
 #include "TBField.h"
 #include "TTree.h"
 #include "ALEvent.h"
 
+
+#include <iostream>
+#include "TNtupleD.h"		  // from ROOT
+#include "TFile.h"			  // from ROOT
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <stdio.h>
+#include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <TROOT.h>
+#include <Riostream.h>
+#include "TChain.h"
+#include "TCanvas.h"
+#include "TObject.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TMultiGraph.h"
+#include "TAxis.h"
+#include "TMath.h"
+#include "TLegend.h"
+#include "TLeaf.h"
+#include "TLine.h"
+#include "TCanvas.h"
+#include "TStyle.h"
+#include "TLinearFitter.h"
+#include "TH2F.h"
+#include "TH2D.h"
+#include "TCut.h"
+#include "TLine.h"
+#include "TArrow.h"
+#include "TBox.h"
+#include "TPDF.h"
+#include "TTree.h"
+#include "TBranch.h"
+#include "TFile.h"
+#include "TPostScript.h"
+#include "TPaveText.h"
+#include "TString.h"
+#include "TH1F.h"
+#include "TSystem.h"
+#include "THStack.h"
+#include "TNtuple.h"
+#include "TFormula.h"
+#include "TFitResultPtr.h"
+#include "TFitResult.h"
+#include "TH3F.h"
+#include "TRandom3.h"
+#include "TRandom2.h"
+#include "TRandom1.h"
+#include "TVector3.h"
+#include "TSpline.h"
+#include "Math/Interpolator.h"
+#include "TMultiGraph.h"
+#include "TPaveText.h"
 #include <iostream>
 class ALKalman {
 
@@ -27,11 +84,10 @@ public:
      ALKalman() {} ; 
 	 virtual ~ALKalman() {};
 	 void InitialBackwardFit(TObjArray &kalhits, THelicalTrack &Hel_1st,TKalMatrix &C_1st);
-	 void Reconstruct();
-	 void Plot();
+	 void GraphAllPoints(ALEvent *re);
+	 void LeastSquares(ALEvent *re);
      int MakeRecoEvent(TBField *bfield, ALEvent *re,int *TckReg);
 	//void Tree_Init();	
-
 	
 	ClassDef (ALKalman, 1)
 		
