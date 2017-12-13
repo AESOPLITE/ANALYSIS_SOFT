7486c13bbde918f80c66a192d28a8a98d7a53a01
smechbal <smechbal@ucsc.edu>
2017-12-13 14:57:15 -0500

    Change eMC to pMC

--- src/ALSim/write99toroot.C
+++ src/ALSim/write99toroot.C
@@ -6,16 +6,55 @@
 void write99toroot()
 {
 
- TFile*file=new TFile("aesoplite.root","RECREATE");
- //TNtuple*ntuple=new TNtuple("BoundX","BoundX","ncase:mreg:newreg:jtrack:ek:xsco:ysco:zsco:atrack:cxtrck:cytrck:cztrck");
- TNtuple*ntuple=new TNtuple("Track","Track","ncase:mreg:mtrack:type:age:e:x:y:z:cx:cy:cz:Edep:flag");
- ntuple->ReadFile("aesoplite001_fort.99");
- 
- //Can read multiple files. Ouputs will be merge in an single file
- // ntuple->ReadFile("aesoplite002_fort.99");
- // ntuple->ReadFile("aesoplite003_fort.99");
+ char* Outpath="rootfiles";
+ char* Inppath=".";
 
- ntuple->Write();
- file->Close();
-  
+	//////////////for electrons/positrons///////// 
+	
+ int type=3; //3 for electrons, 4 for positrons
+ int Nene=12;
+ int Ene[12]={10,20,30,40,50,60,70,80,90,100,200,300};
+ 
+/*
+	////////////for mu+-////////////
+	
+ int type=11;					//10 for mu+
+ int Nene=3;
+ int Ene[3]={1,2,3};
+*/	
+///////////////////////////////////////////////////////	
+ int* Ncycles=new int[Nene]; 
+ for(int i=0;i<Nene;i++)Ncycles[i]=100;  
+ 
+ TFile*file;
+ TNtuple*ntuple;
+ 
+ char* startfile="aesopliteNonUniB_V1";
+ char* endfile="_fort.99";
+
+ for(int i=3;i<4;i++)
+   {
+   // for(int j=0;j<Ncycles[i];j++)
+	   for(int j=0;j<10;j++)
+      {
+		  
+	////////for electrons/////////////////////////
+		  
+       cout << Form("%s/%d/%s_%d_%dMeV%03d%s",Inppath,type,startfile,type,Ene[i],j+1,endfile) <<endl;
+       file=new TFile(Form("%s/%d/%s/%s_%d_%dMeV%03d%s.root",Inppath,type,Outpath,startfile,type,Ene[i],j+1,endfile),"RECREATE");
+       ntuple =new TNtuple("Track","Track","ncase:mreg:mtrack:type:age:p:x:y:z:cx:cy:cz:Edep:flag");
+       ntuple->ReadFile(Form("%s/%d/%s_%d_%dMeV%03d%s",Inppath,type,startfile,type,Ene[i],j+1,endfile));
+		  
+	////////////for muons////////////////////////////	  
+		/*  
+	   cout << Form("%s/%d/%s_%d_%dGeV%03d%s",Inppath,type,startfile,type,Ene[i],j+1,endfile) <<endl;
+       file=new TFile(Form("%s/%d/%s/%s_%d_%dGeV%03d%s.root",Inppath,type,Outpath,startfile,type,Ene[i],j+1,endfile),"RECREATE");
+       ntuple =new TNtuple("Track","Track","ncase:mreg:mtrack:type:age:e:x:y:z:cx:cy:cz:Edep:flag");
+       ntuple->ReadFile(Form("%s/%d/%s_%d_%dGeV%03d%s",Inppath,type,startfile,type,Ene[i],j+1,endfile));
+		  
+		*/  
+       ntuple->Write();
+       file->Close();   
+      }//j
+    }//i
 }
